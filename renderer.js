0ab02aa78f43c3e55d8f4b0b6b790dbd6b81bdaf
PaologGithub <PaologGithub>
2023-11-11 15:55:11 +0100

    New

--- renderer.js
+++ renderer.js
@@ -2,6 +2,7 @@ setTimeout(() => {
     try {
         const electron = require("electron")
         const btn = document.getElementById("jsgame-control_center")
+        btn.onclick = () => {window.location.href = "https://paologgithub.github.io/JSGameControlCenter/index.html"}
         btn.hidden = false;
     } catch (e) {
         console.info("Aren't in JSGame: " + e)
