76357673e51b1a384394f11b020eedb7e415bce1
PaologGithub <PaologGithub>
2023-11-11 15:41:16 +0100

    2 Commit

--- renderer.js
+++ renderer.js
@@ -1,7 +1,9 @@
-try {
-    const electron = require("electron")
-    document.getElementById("jsgame-control_center").hidden = false;
-} catch (e) {
-    console.info("Not in JSGame: " + e)
-    document.getElementById("jsgame-control_center").hidden = true;
-}
+setTimeout(() => {
+    try {
+        const electron = require("electron")
+        document.getElementById("jsgame-control_center").hidden = false;
+    } catch (e) {
+        console.info("Not in JSGame: " + e)
+        document.getElementById("jsgame-control_center").hidden = true;
+    }
+}, 100)
