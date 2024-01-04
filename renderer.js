7251f08cac792a1e8996894fd88eac655e8a0e9b
PaologGithub <PaologGithub>
2024-01-04 14:38:54 +0100

    Makefile Commit

--- renderer.js
+++ renderer.js
@@ -14,7 +14,7 @@ setTimeout(() => {
         document.body.appendChild(styleLink)
 
         const btn = document.getElementById("jsgame-control_center")
-        btn.onclick = () => {window.location.href = "https://paologgithub.github.io/JSGameControlCenter/index.html"}
+        btn.onclick = () => {window.location.href = "https://paologgithub.github.io/jsgame/JSGameControlCenter/index.html"}
         btn.hidden = false;
     } catch (e) {
         console.info("Aren't in JSGame: " + e)
