abaf98957310bd073820e0fa773af1090019462f
PaologGithub <harryolz41@gmail.com>
2024-10-05 16:02:26 +0200

    Testing SvelteKit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+    import { base } from '$app/paths';
+</script>
+
+<h3>Home page</h3>
+<a href="{base}/">About</a>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+    import {base} from '$app/paths';
+</script>
+
+<h3>about</h3>
+<a href="{base}/">Go back</a>
